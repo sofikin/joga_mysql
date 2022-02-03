877ef9455bf428e33cbe77cb5d88a0b9de001b77
sofikin <sofia.kalpin@khk.ee>
2022-02-03 13:48:19 +0200

    “Displayed an article by slug”

--- index.js
+++ index.js
@@ -47,6 +47,20 @@ app.get('/', (req, res) => {
     })
 });
 
+// show articles by this slug
+app.get('/article/:slug', (req, res) => {
+    let query = `SELECT * FROM article WHERE slug="${req.params.slug}"`;
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        article = result
+        console.log(article)
+        res.render('article', {
+            article: article
+        })
+    });
+});
+
 // app start point
 app.listen(3000, () => {
     console.log('App is started at http://localhost:3000');
